aa3028674caf1e72e9e8bc7caf7d468adb03a277
Isac pacheco <isac.pachecolopez@unicolombo.edu.co>
2024-04-23 17:57:23 -0500

    Cuadros de verificacion

---
+++ guia_1/src/paquetePrincipal/Principal.java
@@ -0,0 +1,21 @@
+
+package paquetePrincipal;
+
+import paqueteVista.MainWindow;
+
+/**
+ *
+ * @author isac pacheco
+ */
+public class Principal {
+public static void main(String[] args){
+        
+    
+MainWindow ventana = new MainWindow();
+
+ventana.setLocationRelativeTo(null);
+ventana.setVisible(true);
+        
+    }
+    
+}
